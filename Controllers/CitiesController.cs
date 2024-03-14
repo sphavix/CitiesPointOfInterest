36dbbd3fea24c14c5d5c4b3fec016cc443b2102e
Siphamandla Zolakhe <zwelakhespha@gmail.com>
2024-03-14 15:17:12 +0200

    first commit

---
+++ Controllers/CitiesController.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace CityPointOfInterest.Controllers
+{
+    [ApiController]
+    [Route("api/cities")]
+    public class CitiesController : ControllerBase
+    {
+        [HttpGet]
+        public JsonResult GetCities()
+        {
+            return new JsonResult(CitiesDataStore.Current.Cities);
+        }
+
+        [HttpGet("{id}")]
+        public JsonResult GetCity(int id)
+        {
+            return new JsonResult(CitiesDataStore.Current.Cities.FirstOrDefault(x => x.Id == id));
+        }
+    }
+}
