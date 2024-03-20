1bc24a562ad91e7f73dad5724ad6e8220d4bc329
Siphamandla Zolakhe <zwelakhespha@gmail.com>
2024-03-20 09:52:19 +0200

    Modified assembly

--- obj/Debug/net8.0/CityPointOfInterestApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/CityPointOfInterestApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CityPointOfInterestApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd53708e4e4eb8684edef5010eb13867fc6ab396")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6d4a948a8e98c9e35a05d54513d9531649db5de")]
 [assembly: System.Reflection.AssemblyProductAttribute("CityPointOfInterestApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CityPointOfInterestApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
